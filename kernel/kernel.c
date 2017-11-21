7839f3560dc173dc377bc624be22bdbf6f094d3e
Pablo <pabloorduna98@gmail.com>
2017-11-21 14:31:11 +0100

    Improve c code structure

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -4,31 +4,40 @@
  * File: kernel/kernel.c
  */
 
-void kernel_main(void){
-	int i = 0;
+
+// 80 columns, 25 lines, 2 bytes per element
+#define SCREEN_BYTE_SIZE 80 * 25 * 2
+
+char *video_ptr = (char*)0xb8000;
+
+
+void clear_screen(){
 	char clear = ' ';
-	// 80 columns, 25 lines, 2 bytes per element
-	int screen_byte_size = 80 * 25 * 2;
-	char *video_ptr = (char*)0xb8000;
+	int i = 0;
+	while(i < SCREEN_BYTE_SIZE){
+                video_ptr[i] = clear;
+
+                /* 0x07
+                 * 0: Background: Black
+                 * 7: Font color: Grey
+                 */
+                video_ptr[i+1] = 0x07;
+                i += 2;
+        }
+}
+
+void kernel_main(void){
 	const char *welcome_text = "Welcome to Wildfire Project";
 	
 	// Clear screen
 
-	while(i < screen_byte_size){
-		video_ptr[i] = clear;
-		
-		/* 0x07
-		 * 0: Background: Black
-		 * 7: Font color: Grey
-		 */
-		video_ptr[i+1] = 0x07;
-		i += 2;
-	}
-	int j = 0;
-	i = 0;
+	clear_screen();
 
 	// Write text from the welcome_text string
 	
+	int i = 0;
+	int j = 0;
+
 	while(welcome_text[j] != '\0'){
 		video_ptr[i] = welcome_text[j];
 		video_ptr[i+1] = 0x07;
